03c8c9d871a9055baec7b9c8a0c8d5c76b5a166f
light94 <mishra.rahul1712@gmail.com>
2015-10-03 07:10:45 +0530

    Removed unnecessary code and spaces

--- rotnotes/rotnotes.py
+++ rotnotes/rotnotes.py
@@ -2,22 +2,19 @@ import os
 def rename(directory,rot= False):
 	os.chdir(directory)
 	e = os.listdir(directory)
-	
 	e.sort() 
-
 	a = len(e)
 
 	for i in range(0,a):
 		os.rename(e[i],str(i+1)+".jpg")
 
-	if rot!=False:
+	if rot:
 		try:
 			from PIL import Image
 		except:
 			return 'You have not installed all dependencies for the app. Please cd to the location where you downloaded the project and do pip install -r requirements.txt'
 
 		files = os.listdir(os.getcwd())
-
 		for file in files:
 			im = Image.open(file)
 			length,breadth = im.size
